6eface9c185d7d933cf173893721198940e8180e
Nelson Roque <nelsonroquejr@gmail.com>
2022-02-15 16:13:43 -0500

    update to include devtools install

--- FIRST_RUN_REQUIRED_PACKAGES.R
+++ FIRST_RUN_REQUIRED_PACKAGES.R
@@ -1,5 +1,8 @@
-# list of packages required on machine if developing on this R Package
-packages_req_dev = c("rlist", "skimr", "tidyverse") # optional: "dataReporter"
+# list of packages required on machine if developing on this R Package ---
+packages_req_dev = c("rlist", "skimr", "tidyverse", "devtools") # optional: "dataReporter"
 
-# install them
+# install them ---
 install.packages(packages_req_dev)
+
+# install `wearitR` package ---
+devtools::install_github("nelsonroque/wearitR")
