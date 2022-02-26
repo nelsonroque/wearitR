c1edec5c9e135357b925ab5224e242f1e943a34d
Nelson Roque <nelsonroquejr@gmail.com>
2022-02-26 10:56:20 -0500

    components of what will be read study config function

--- todo/read_studyconfig.R
+++ todo/read_studyconfig.R
@@ -1,5 +1,17 @@
 #' Read WearIT Survey data ----
 #' @export
+read_surveyconfig <- function(config_path) {
+  
+  # create session timestamp ----
+  session_ts = str_replace_all(Sys.time(), "[[:punct:]]", "_")
+  
+  # read study config ----
+  study_config = jsonlite::read_json(config_path)
+  
+  study_config$study_groupcode
+    
+# https://wearables.vmhost.psu.edu/wearables-survey/Survey/blockStructure?s_id=65&g_id=1035
+# https://wearables.vmhost.psu.edu/wearables-survey/Survey/group/1035
 
 # configure API endpoints
 base_url = "https://wearables.vmhost.psu.edu/wearables-survey/Survey/"
@@ -20,3 +32,5 @@ jsonlite::toJSON(m_df)
 jsonlite::toJSON(s_df)
 
 
+  return()
+}
